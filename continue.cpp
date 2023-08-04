60d34f3a336be64c5e52337eb5f5b7e19df6b894
0xF11 <140972657+0xF11@users.noreply.github.com>
2023-08-04 13:51:05 -0400

    Update continue.cpp

--- continue.cpp
+++ continue.cpp
@@ -5,20 +5,48 @@ int continuestatement(int cont)
 {
 	for (int i{ 0 }; i <= cont; ++i)
 	{
-		if (i % 2 == 0)
+		if (i == 5)
 		{
 			continue;// jumps to the next itteration in the loop rather than terminating the loop or function
 		}
-		
+
 		std::cout << i;
 		std::cout << '\n';
+
+
+
 	}
 	return 0;
 }
 
+int breakwhile(int breakcont)
+{
+	while (breakcont < 10)
+	{
+		if (breakcont == 5)
+		{
+			continue;
+		}
+		std::cout << '\n';
+
+		std::cout << breakcont;
+		breakcont++;
+	}
+
+	return 0;
+}
 
 int main()
 
 {
 	continuestatement(11);
+	breakwhile(0);
+
+
+
+	std::cout << '\n';
+
+
+
+
 }
